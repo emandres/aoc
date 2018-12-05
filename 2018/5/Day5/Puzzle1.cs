d6b7757617046f53b058f81733b64c34734b0121
Eric Andres <eric-andres@pluralsight.com>
2018-12-05 08:27:23 -0500

    Day 5

---
+++ 2018/5/Day5/Puzzle1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day5
+{
+    public class Puzzle1
+    {
+        public void Execute()
+        {
+            var units = File.ReadAllText("../../../input.txt");
+            Console.WriteLine(units.Length);
+            var stack = new Stack<char>();
+            foreach (var unit in units)
+            {
+                TryReacting(unit, stack);
+            }
+
+            Console.WriteLine(stack.Count);
+        }
+
+        void TryReacting(char unit, Stack<char> stack)
+        {
+            if (stack.TryPeek(out char top) && AreReactive(unit, top))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(unit);
+            }
+        }
+
+        bool AreReactive(char a, char b)
+        {
+            return a != b && char.ToLower(a) == char.ToLower(b);
+        }
+    }
+}
---
+++ 2018/5/Day5/Puzzle2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day5
+{
+    public class Puzzle2
+    {
+
+        public void Execute()
+        {
+            var units = File.ReadAllText("../../../input.txt");
+            Console.WriteLine(units.Length);
+            int min = Int32.MaxValue;
+            for (char bannedType = 'a'; bannedType <= 'z'; bannedType++)
+            {
+                var stack = new Stack<char>();
+                foreach (var unit in units)
+                {
+                    TryReacting(unit, stack, bannedType);
+                }
+
+                if (stack.Count < min)
+                {
+                    min = stack.Count;
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+
+        void TryReacting(char unit, Stack<char> stack, char bannedType)
+        {
+            if (char.ToLower(unit) == bannedType)
+            {
+                return;
+            }
+
+            if (stack.TryPeek(out char top) && AreReactive(unit, top))
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(unit);
+            }
+        }
+
+        bool AreReactive(char a, char b)
+        {
+            return a != b && char.ToLower(a) == char.ToLower(b);
+        }
+    }
+}
